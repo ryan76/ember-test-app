b96852b8ccf805ec4efe74e8446ec9a3fc3a7346
Ryan Niemann <rniemann@iparadigms.com>
2016-01-28 12:53:52 -0600

    Updated comment

--- app/components/value-gauge/component.js
+++ app/components/value-gauge/component.js
@@ -5,14 +5,14 @@ export default Ember.Component.extend({
   classNameBindings: ['isMaxValueExceeded:exceeded'],
 
   isMaxValueExceeded: function(){
-    // parseInt because we want Numbers and inputs value actually are Strings
+    // parseInt because we want ensure input values are Numbers or String representation of numbers
     var value    = parseInt(this.get('value'), 10);
     var maxValue = parseInt(this.get('maxValue'), 10);
     return (value > maxValue); // return a Boolean
   }.property('value', 'maxValue'),
 
   computedAngle: function(){
-    // parseInt because we want Numbers and inputs value actually are Strings
+    // parseInt because we want ensure input values are Numbers or String representation of numbers
     var value    = parseInt(this.get('value'), 10);
     var maxValue = parseInt(this.get('maxValue'), 10);
 
