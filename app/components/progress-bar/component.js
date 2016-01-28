59ab493aec1f59b62803c36754137759739052d2
Ryan Niemann <rniemann@iparadigms.com>
2016-01-28 16:46:10 -0600

    Added inputs for progress bar.

--- app/components/progress-bar/component.js
+++ app/components/progress-bar/component.js
@@ -3,16 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ["progress-bar"],
 
-  computeValue: function() {
-    var value = parseInt(this.get('value'), 10);
-    return value;
-  }.property('value'),
-
   computeText: function() {
-    var maxValue = parseInt(this.get('maxValue'), 10);
     var value = parseInt(this.get('value'), 10);
+    var maxValue = parseInt(this.get('maxValue'), 10);
     var percent = value/maxValue * 100;
     var text = percent.toString() + '%';
     return text;
-  }.property('text')
+  }.property('value', 'maxValue')
 });
