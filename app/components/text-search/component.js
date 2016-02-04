d67ddd639aa7b743f9d4affd4d80ea80509c0bf1
Ryan Niemann <rniemann@iparadigms.com>
2016-02-04 12:29:51 -0600

    Added some styling for the text-search component and modified the markup.

--- app/components/text-search/component.js
+++ app/components/text-search/component.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ["text-search"],
+  classNames: ["text-search", "component-listing"],
   textToFind: null,
   textToReplace: null,
 
   actions: {
     findText() {
-      $('.text-search').blast({delimiter: "word", generateValueClass: true});
+      $('.text-search .component-container p').blast({delimiter: "word", generateValueClass: true});
       var createdClass = '.blast-word-' + this.textToFind;
       $(createdClass).css('color', 'red');
     },
@@ -15,6 +15,7 @@ export default Ember.Component.extend({
     replaceText() {
       var createdClass = '.blast-word-' + this.textToFind;
       $(createdClass).text(this.textToReplace);
+      $(createdClass).css('color', 'green');
     }
   }
 });
