bdd33398f60ff9c0cbcbfc0912d0f040f3a7c171
Ryan Niemann <rniemann@iparadigms.com>
2016-02-04 14:44:03 -0600

    Added separate route for text-search.

--- app/components/text-search/component.js
+++ app/components/text-search/component.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ["text-search", "component-listing"],
+  classNames: ["text-search"],
   textToFind: null,
   textToReplace: null,
 
   actions: {
     findText() {
       var createdClass = '.blast-word-' + this.textToFind;
-      $('.text-search .component-container p').blast({delimiter: "word", generateValueClass: true});
+      $('.text-search .search-container p').blast({delimiter: "word", generateValueClass: true});
       $(createdClass).css('background-color', 'rgba(255, 255, 0, 0.5)');
     },
 
     replaceText() {
       var createdClass = '.blast-word-' + this.textToFind;
       $(createdClass).text(this.textToReplace);
-      $('.text-search .component-container p').blast(false);
+      $('.text-search .search-container p').blast(false);
     }
   }
 });
